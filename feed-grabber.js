9af8c7ff9197f934f101ef78320580bb9554958a
mssen <missy.danielsen@gmail.com>
2018-02-27 23:25:07 -0600

    Formatted threshold to time in MS and return feed count on success.

--- feed-grabber.js
+++ feed-grabber.js
@@ -1,17 +1,16 @@
 import subHours from 'date-fns/sub_hours';
+import format from 'date-fns/format';
 
 import { listFeeds } from './lib/dynamo';
 import { sendFeed } from './lib/sns';
 
 export async function main(event, context, callback) {
   try {
-    // const feeds = await listFeeds(subHours(Date.now(), 12));
-    const feeds = {
-      Items: [{ Url: 'feed1', LastUpdatedAt: Date.now() }]
-    };
+    const feeds = await listFeeds(format(subHours(Date.now(), 12), 'x'));
     feeds.Items.forEach(
       async (feed) => await sendFeed(feed.Url, feed.LastUpdatedAt)
     );
+    callback(null, `Sent ${feeds.Count} feeds.`);
   } catch (error) {
     callback(error);
   }
