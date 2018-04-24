5ccf10a216f9b3c393c974c4ee150c25b734e8a7
mssen <missy.danielsen@gmail.com>
2018-04-24 12:47:29 -0500

    Updated to Node 8.10 with async/await.

--- src/handler/feed-grabber.js
+++ src/handler/feed-grabber.js
@@ -5,13 +5,16 @@ const { formatDate } = require('../lib/util');
 const { listFeeds, updateLastUpdatedFeed } = require('../lib/dynamo');
 const { sendFeed } = require('../lib/sns');
 
-module.exports.main = (event, context, callback) => {
+module.exports.main = async (event, context, callback) => {
   const now = Date.now();
 
-  listFeeds(formatDate(subHours(12, now)))
-    .then(({ Items }) => updateFeeds(Items, now))
-    .then(() => callback(null, 'Successfully sent feeds.'))
-    .catch(callback);
+  try {
+    const feeds = await listFeeds(formatDate(subHours(12, now)));
+    await updateFeeds(feeds.Items, now);
+    callback(null, `Successfully sent ${feeds.Count} feed(s).`);
+  } catch (error) {
+    callback(error);
+  }
 };
 
 const updateFeeds = (feeds, now) =>
